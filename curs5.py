1c4cde70582687fcb1405c335cbf2aa9cd419889
Cristiana <cristiana15_97@yahoo.com>
2024-12-16 21:03:11 +0200

    curs 5

---
+++ curs5.py
@@ -0,0 +1,21 @@
+# # # clienti=[]
+# # # n=int(input("Cati clienti are firma dumneavoastra?"))
+# # # for i in range (1, n+1):
+# # #     client=input(f"Introduceti cliemtul cu numarul{i}")
+# # #     clienti.append(client)
+# # # print(clienti)
+# # it = ['Rares' , 'Radu' , 'Cristina']
+# # marketing = ['Victor' , 'Ioana']
+# # meeting=[]
+# # meeting.extend(it)
+# # meeting.extend(marketing)
+# # meeting.remove('Radu')
+# # print(meeting)
+# numere=[9,4,1,12,5,4]
+# numere.sort()
+# print(numere)
+# numere.pop(1)
+# numere.pop()
+# print (max(numere))
+# print(len(numere))
+
